ad4cad757108614449ac0f89b616a1e70df37489
StarikTenger <StarikTenger@gmail.com>
2022-11-26 23:01:09 +0300

    add range works

--- guita/Cursor.js
+++ guita/Cursor.js
@@ -13,6 +13,22 @@ class Cursor {
 	setType(_type) {
 		this.type = _type
 	}
+
+	setTower(pos) {
+		console.log(pos)
+		if (this.type = CURS_RANGE) {
+			let range = document.createElement("input")
+			range.class = "tower"
+			range.type = "range";
+			range.style.position = "absolute";
+			range.style.left = pos.x;
+			range.style.top = pos.y;
+			console.log(range.style.position);
+			document.getElementById("towers").append(range)
+		}
+		
+		this.type = CURS_NONE;
+	}
 }
 
 let cursor = new Cursor();
@@ -20,3 +36,5 @@ let cursor = new Cursor();
 document.getElementById("add_range").onclick = function(){cursor.setType(CURS_RANGE)}
 document.getElementById("add_textfield").onclick = function(){cursor.setType(CURS_TEXTBOX)}
 document.getElementById("add_radiobuttons").onclick = function(){cursor.setType(CURS_RADIOBUTTONS)}
+
+SCREEN.onclick = function(event){cursor.setTower(new Vec2(event.pageX, event.pageY))};
